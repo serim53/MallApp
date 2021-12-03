d98f47a8ac35a7efa23668ab0995e1887433e579
serim53 <tpfla9953@naver.com>
2021-12-03 22:40:48 +0900

    도커에 장고만 담기

--- shoppingmallProj/settings.py
+++ shoppingmallProj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-304=ru)iy2eii%j#%v@mz&uhr7o_e39$o(7=yydtg_pq$jl&g-'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-304=ru)iy2eii%j#%v@mz&uhr7o_e39$o(7=yydtg_pq$jl&g-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else :
+    ALLOWED_HOSTS = []
 
 
 # Application definition
