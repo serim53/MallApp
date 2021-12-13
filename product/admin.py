58baa12c2afbef642ee35e6adfdee0d233e9fffb
serim53 <tpfla9953@naver.com>
2021-12-13 17:59:11 +0900

    category 뷰 추가/ Comment 모델 추가

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Product, Category
+from product.models import Product, Category, Comment
 
 admin.site.register(Product)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment)
--- product/models.py
+++ product/models.py
@@ -1,8 +1,11 @@
+import os.path
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.urls import reverse
-
+from django_markdown.utils import markdown
+from markdownx.models import MarkdownxField
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -19,7 +22,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_in_category', args=[self.slug])
+        return f'/product/category/{self.slug}'
 
 
 class Product(models.Model):
@@ -47,4 +50,25 @@ class Product(models.Model):
 
     def get_absolute_url(self):
 
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return f'/blog/{self.pk}/'
+
+    def get_content_markdown(self):
+        return markdown(self.description)
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.product.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists() :
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else :
+            return 'https://doitdjango.com/avatar/id/435/34def6c20b3733a7/svg/{self.author.email}/'
